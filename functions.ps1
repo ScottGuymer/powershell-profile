d2aa7a3caf1eddcfc95540dce4cc1a9954aead56
Scott Guymer <scott.guymer@philips.com>
2021-12-01 15:13:32 +0100

    Added some more functiona

--- functions.ps1
+++ functions.ps1
@@ -9,15 +9,8 @@ function Set-NVMVersion {
     Write-Host ".nvmrc file does not exist in this directory."
   }
  }
- 
-function Clear-ZScaler {
-  $regKey="HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
-  Set-ItemProperty -path $regKey AutoConfigURL -Value ""
-  #http://pac.zscalertwo.net/philips.com/global-pac.pac
-  Write-Host "ZScaler Proxy Script Cleared" -ForegroundColor Green
-}
 
-function touch ($filename) {
+function touch($filename) {
   New-Item -ItemType file $filename
 }
 
@@ -30,4 +23,49 @@ function Optimize-Docker-Images {
   docker rm $(docker ps -a -q)
   Write-Host "Removing all dangling images" -ForegroundColor Green
   docker rmi $(docker images --filter "dangling=true" -a -q)
+}
+
+function base64encode() {
+  [CmdletBinding()]
+  Param(
+    [Parameter(ValueFromPipeline)] $Text
+  )
+  $Bytes = [System.Text.Encoding]::Unicode.GetBytes($Text)
+  $EncodedText =[Convert]::ToBase64String($Bytes)
+  return $EncodedText
+}
+
+function base64decode() {
+  [CmdletBinding()]
+  Param(
+    [Parameter(ValueFromPipeline)] $Text
+  )
+  $Bytes = [Convert]::FromBase64String($Text)
+  $DecodedText = [System.Text.Encoding]::Unicode.GetString($Bytes)
+  return $DecodedText
+}
+
+function aws_role([String]$role) {
+
+  $env:_AWS_ACCESS_KEY_ID=$env:AWS_ACCESS_KEY_ID
+  $env:_AWS_SECRET_ACCESS_KEY=$env:AWS_SECRET_ACCESS_KEY
+  $env:_AWS_SESSION_TOKEN=$env:AWS_SESSION_TOKEN
+
+  $aws_credentials=$(aws sts assume-role --role-arn $role --role-session-name $($(whoami) -Replace '[^a-zA-Z0-9]',''))
+
+  $json = $aws_credentials | ConvertFrom-Json
+  $env:AWS_ACCESS_KEY_ID=$json.Credentials.AccessKeyId
+  $env:AWS_SECRET_ACCESS_KEY=$json.Credentials.SecretAccessKey
+  $env:AWS_SESSION_TOKEN=$json.Credentials.SessionToken
+}
+
+function aws_clear_role([String]$role) {
+
+  $env:AWS_ACCESS_KEY_ID=$env:_AWS_ACCESS_KEY_ID
+  $env:AWS_SECRET_ACCESS_KEY=$env:_AWS_SECRET_ACCESS_KEY
+  $env:AWS_SESSION_TOKEN=$env:_AWS_SESSION_TOKEN
+
+  $env:_AWS_ACCESS_KEY_ID=""
+  $env:_AWS_SECRET_ACCESS_KEY=""
+  $env:_AWS_SESSION_TOKEN=""
 }
